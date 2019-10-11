a6ab33b81c4c35aebd8ca8b62fec8e1ee3d4fe74
BenoitD34 <be.dugovic@gmail.com>
2019-10-11 16:12:21 +0200

    merge folders

---
+++ TP_05JS.js
@@ -0,0 +1,8 @@
+$carousel = document.getElementById("carousel");
+$suivant = document.getElementById("suivant");
+// Vous aurez besoin de ce genre de choses :
+// $carousel.style.borderColor = "#f00";
+function suivant() {
+    // ...
+}
+$suivant.onclick = suivant;
